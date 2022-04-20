c1d7e7af1d8897ad1896fe921cc2ca5705d7e4ca
Rozyakhter <rozyakhter7@gmail.com>
2022-04-20 16:05:28 -0400

    Remove code

--- src/test/java/orangeHrmPom/TestingLogIn.java
+++ src/test/java/orangeHrmPom/TestingLogIn.java
@@ -1,12 +1,10 @@
 package orangeHrmPom;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
-
 import pages.AdminPage;
 import pages.DashBoardPage;
 import pages.LogInPage;
